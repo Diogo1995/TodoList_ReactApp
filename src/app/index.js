bc02c6e264c054639803bd48b119cf1eee284c3c
Diogo Pedrosa <diogo.pedrosa@innowave.tech>
2018-10-01 16:54:37 +0100

    step 5

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,17 @@
-import App from './app.component';
+
+import React from "react";
+
+import env from '@env';
+
+const App = () => {
+    console.log('env', env);
+    return (
+        <div className="container">                                     
+            <div className="col-12">                                    
+                <h1 className="text-center">Good Morning Vietnam!</h1>  
+            </div>                                                      
+        </div>                                                          
+    )
+};
 
 export default App;
