1d6795fcaf47f2361398e214c31de313636d7719
Diogo Pedrosa <diogo.pedrosa@innowave.tech>
2018-10-02 11:19:06 +0100

    step 6

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,14 +1,45 @@
 import React from "react";
+import {                                                        
+    BrowserRouter as Router,                                         
+    Route,                                                            
+    Link,                                                             
+    Switch                                                           
+} from 'react-router-dom';
+import loadable from 'loadable-components';
 
 import env from '@env';
+const Home = loadable(() => import('./home'));             
+const About = loadable(() => import('./about'));          
+const TodoApp = loadable(() => import('./todo-app'))                          
 
-export default () => {
+const App = () => {
     console.log('env', env);
     return (
-        <div className="container">                                     
-            <div className="col-12">                                    
-                <h1 className="text-center">Hello, World!</h1>  
-            </div>                                                      
-        </div>                                                          
+        <div className="container">
+            <div className="col-12">
+                <h1>Hello, world!</h1>
+
+                <Router>                                                
+                    <div>                                               
+                        <ul>                                            
+                            <li><Link to="/">Home</Link></li>           
+                            <li><Link to="/about">About</Link></li>     
+                            <li><Link to="/todo-app">TodoApp</Link></li>      
+                        </ul>                                           
+                                                                        
+                        <hr/>                                           
+
+                        <Switch>                                        
+                            <Route exact path="/" component={Home}/>    
+                            <Route path="/about" component={About}/>    
+                            <Route path="/todo-app" component={TodoApp}/>      
+                        </Switch>                                       
+                    </div>                                              
+                </Router>                                               
+
+            </div>
+        </div>
     )
 };
+
+export default App;
